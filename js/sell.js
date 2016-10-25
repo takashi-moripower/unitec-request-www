4dde4f620a3c6bd0cd0b22e3202a46cc9274e837
takashi-moripower <takashi@moripower.jp>
2016-10-25 09:12:46 +0900

    1025

--- js/sell.js
+++ js/sell.js
@@ -8,8 +8,17 @@ $(function () {
 function checkCount() {
 	sum = 0;
 	$('select.count').each(function(i,e){
+		
+		check = $(e).parents('tr').find('th.check');
+		
+		val = $(e).val();
+		if( val >= 1 ){
+			check.html('<i class="fa fa-check-square-o fa-fw fa-2x"></i>');
+		}else{
+			check.html('<i class="fa fa-square-o fa-fw fa-2x"></i>');
+		}
 
-		sum += Number( $(e).val() );
+		sum += Number( val );
 	});
 	
 	if( sum > 0 ){
